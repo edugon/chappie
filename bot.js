da73f9030eb46fbc79381c42912f086992be00c0
User <egonrea@gmail.com>
2018-11-26 20:19:59 +0000

    add project

---
+++ bot.js
@@ -0,0 +1,49 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+
+client.on("guildMemberAdd", function (member) {
+  console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
+  member.guild.channels.get("putoscasual").send(`Hello World! "${member.user.username}" ` +
+    `just joined... Welcome! come in and wash the dishes hehe regards`);
+});
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !regards
+            case 'regards':
+                bot.sendMessage({
+                    to: channelID,
+                    message: `Heey yoooo "${user.username}"! Regards!!`
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+})
