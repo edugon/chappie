fb2ffb31ede558b21611d4ae414ada930cb7b04b
User <egonrea@gmail.com>
2018-12-03 19:50:45 +0000

    add multilanguage

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,6 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js"),
+	  config = require('./config.json'),
+	  texts = require('./texts.json');
 
 exports.colors = {
 	DARK_GREEN: 'DARK_GREEN',
@@ -6,26 +8,28 @@ exports.colors = {
 	DARK_GOLD: 'DARK_GOLD'
 }
 
+exports.icons = {
+	INFO: 'http://www.myiconfinder.com/uploads/iconsets/256-256-92772c80d46c3d241a175d4ade309f88.png'
+}
+
 exports.gifs = {
 	REGARDS: 'https://media2.giphy.com/media/pjZLhQIEx9dBe/giphy.gif',
 	WALKING: 'https://media.giphy.com/media/KWjRQ4Zttlzb2/giphy.gif'
 }
 
-exports.footers = {
-	INFO: 'Type "!chappie info" to know more about me 😊',
-	WIP: 'This may change in the future ☝',
-	MAINTAINER: 'I have been developed by eduu <shockeR>'
-}
-
 exports.createEmbed = function(title, author, avatar, color, description, image, 
-	footer, thumbnail) {
+	footer, thumbnail, icon) {
+	let authorSay = null;
+	if(author != null) {
+		authorSay = author + texts[config.lang].titles.say;
+	}
 	let embed = new Discord.RichEmbed()
 		.setTitle(title != null ? title : '')
-		.setAuthor(author != null ? author + ' says:' : '', avatar != null ? avatar : '')
+		.setAuthor(authorSay != null ? authorSay : '', avatar != null ? avatar : '')
 		.setColor(color != null ? color : '')
 		.setDescription(description != null ? description : '')
 		.setImage(image != null ? image : '')
-		.setFooter(footer != null ? footer : '')
+		.setFooter(footer != null ? footer : '', icon != null ? icon : '')
 		.setThumbnail(thumbnail != null ? thumbnail : '')
 		.setTimestamp();
 
